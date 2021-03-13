3a10eca47f2df19a44ecd3665de0b881120f1e22
dbouidaine <hd_bouidaine@esi.dz>
2021-03-13 06:49:35 +0100

    footer

--- views/componentsHTML/footer.php
+++ views/componentsHTML/footer.php
@@ -1,7 +1,23 @@
 <footer class="footer bg-light text-dark p-4 b-0">
     <div class="container">
         <center>
-        <p>Pour toute information concernant nos formations :</p>
+            <div class="d-flex flex-row justify-content-center footer">
+                <a class="p-3" href="<?=url('');?>">Accueill</a>
+                <a class="p-3" href="<?=url('ecole/presentation');?>">Présentation</a>
+                <a class="p-3" href="<?=url('ecole/cycle-primaire');?>">Cycle Primaire</a>
+                <a class="p-3" href="<?=url('ecole/cycle-secondaire');?>">Cycle Moyen</a>
+                <a class="p-3" href="<?=url('ecole/cycle-secondaire');?>">Cycle Secondaire</a>
+                <a class="p-3" href="<?=url('space/student');?>">Espace Elèves</a>
+                <a class="p-3" href="<?=url('space/parent');?>">Espace Parents</a>
+                <a class="p-3" href="<?=url('contact');?>">Contacts</a>
+            </div>
+            <div class="d-flex flex-row justify-content-center">
+                <a class="nav-link text-dark my-btn" target="_blanc" href="https://www.facebook.com" class="text-light"><i class="fab fa-facebook fa-2x"></i></a>
+                <a class="nav-link text-dark my-btn" target="_blanc" href="https://www.facebook.com" class="text-light"><i class="fab fa-instagram fa-2x"></i></a>
+                <a class="nav-link text-dark my-btn" target="_blanc" href="https://www.facebook.com" class="text-light"><i class="fab fa-instagram fa-2x"></i></a>
+                <a class="nav-link text-dark my-btn" target="_blanc" href="https://www.facebook.com" class="text-light"><i class="fab fa-instagram fa-2x"></i></a>
+            </div>
+        
         </center>
     </div>
 </footer>
